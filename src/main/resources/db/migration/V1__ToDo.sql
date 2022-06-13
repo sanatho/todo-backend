2613ed344254fd82b3f4d87c1fb1218263c668d8
Thomas <tsanavia@gmail.com>
2022-06-13 15:25:50 +0200

    database table and settings

---
+++ src/main/resources/db/migration/V1__ToDo.sql
@@ -0,0 +1,7 @@
+CREATE TABLE todo(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY ,
+    user_id VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    priority INT NOT NULL
+)
