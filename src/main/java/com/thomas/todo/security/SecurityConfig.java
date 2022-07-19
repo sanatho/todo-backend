07b02c841ed09a1970d2126b45150bcffc8a4516
Thomas <tsanavia@gmail.com>
2022-07-19 14:07:19 +0200

    security configuration edited

--- src/main/java/com/thomas/todo/security/SecurityConfig.java
+++ src/main/java/com/thomas/todo/security/SecurityConfig.java
@@ -28,17 +28,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         This is where we configure the security required for our endpoints and setup our app to serve as
         an OAuth2 Resource Server, using JWT validation.
         */
-//        http.authorizeRequests()
-//                .antMatchers("/api/public").permitAll()
-//                .antMatchers("/api/todo/**").permitAll()
-//                .antMatchers("/api/todo").permitAll()
-//                .antMatchers("/api/private").authenticated()
-//                .antMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
-//                .and().cors()
-//                .and().oauth2ResourceServer().jwt();
-		http
-				.cors().and().csrf().disable()
-				.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests()
+                .antMatchers("/api/public").permitAll()
+//                .antMatchers("/api/v1/todo/*").permitAll()
+//                .antMatchers("//api/v1/todo/**").permitAll()
+                .antMatchers("/api/private").authenticated()
+                .antMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
+                .and().cors()
+                .and().oauth2ResourceServer().jwt();
     }
 
     @Bean
